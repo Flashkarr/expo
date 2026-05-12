44cb6167ffa350c25756d58eabff5666700b26b2
Flashkarr <fedya.xxxgg@gmail.com>
2026-05-12 17:43:01 +0300

    дз 1

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,98 +1,97 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Image, StyleSheet, Text, TextInput, View } from 'react-native';
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
+export default function App() {
+  const [text1, setText1] = useState('');
+  const [text2, setText2] = useState('');
+  const [result, setResult] = useState('');
 
-export default function HomeScreen() {
+  const [image, setImage] = useState(require('../../assets/images/avatar.png'));
+
+  const changeImage = () => {
+  setImage(require('../../assets/images/67.jpg'));
+  };
+  const handlePress = () => {
+    const sum = Number(text1) + Number(text2);
+    setResult(sum.toString());
+  };
+
+    const[count, setCount] = useState(0);
+
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
+    <View style={styles.container}>
+      <Text style={styles.text}>Завдання 1</Text>
 
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+      <TextInput
+        style={styles.input}
+        placeholder="Перше число"
+        value={text1}
+        onChangeText={setText1}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Друге число"
+        value={text2}
+        onChangeText={setText2}
+      />
+
+      <Button title="Натисни" onPress={handlePress} />
+
+      <Text style={styles.text}>Результат: {result}</Text>
+
+
+
+
+      <Text style={styles.text}>Завдання 2</Text>
+
+      <Image source={image} style={styles.image} />
+
+      <Button
+        title="Натисни, щоб змінити картинку"
+        onPress={changeImage}
+      />
+
+
+
+
+      <Text style={styles.text}>Завдання 3</Text>
+      <Text>Лічильник: {count}</Text>
+      <Button title="Додати +1" onPress={() =>setCount(count + 1)} />
+
+    </View>
   );
-}
+};
+
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
-    gap: 8,
+    padding: 20,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+
+  input: {
+    width: '90%',
+    borderWidth: 1,
+    borderColor: 'gray',
+    padding: 10,
+    marginBottom: 15,
+    borderRadius: 5,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+
+  text: {
+    fontSize: 22,
+    marginTop: 20,
+    marginBottom: 10,
+    textAlign: 'center',
+  },
+
+  image: {
+    width: 200,
+    height: 200,
+    marginBottom: 20,
   },
 });
